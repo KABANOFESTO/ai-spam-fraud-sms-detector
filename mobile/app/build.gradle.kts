104ef2a1ca8eb6dce2ede8f73661db1c26e7feab
KABANOFESTO <kabanofesto1@gmail.com>
2026-07-25 13:37:28 +0200

    fix:gradle / settings

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.compose.runtime.saveable)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.activity.compose)
